28873afef79b2982397aa63f70db78c500e0e8db
Tam Dao <tamdc8897@gmail.com>
2021-09-30 23:51:47 +0700

    Cập nhập lại domain ứng dụng v1

---
+++ Tshopper.Data/Entities/District.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Tshopper.Data.Entities
+{
+    [Table("District")]
+    public class District : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public Guid ProvinceId { get; set; }
+
+        [ForeignKey("ProvinceId")]
+        public Province Province { get; set; }
+
+        public ICollection<Ward> wards { get; set; }
+    }
+}
---
+++ Tshopper.Data/Entities/Province.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tshopper.Data.Entities
+{
+    [Table("Province")]
+    public class Province : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<District> Districts { get; set; }
+    }
+}
